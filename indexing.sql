875657e9a478ed6c16e4bf8a4e3cd7949fb7c46f
Lisa <misslisazhou@gmail.com>
2016-07-16 19:41:11 -1000

    complete set up

---
+++ indexing.sql
@@ -0,0 +1,17 @@
+DROP DATABASE IF EXISTS indexed_cars;
+DROP USER IF EXISTS indexed_cars_user;
+CREATE USER indexed_cars_user;
+CREATE DATABASE indexed_cars WITH OWNER = indexed_cars_user;
+
+\i scripts/car_models.sql;
+\i scripts/car_model_data.sql;
+\i scripts/car_model_data.sql;
+\i scripts/car_model_data.sql;
+\i scripts/car_model_data.sql;
+\i scripts/car_model_data.sql;
+\i scripts/car_model_data.sql;
+\i scripts/car_model_data.sql;
+\i scripts/car_model_data.sql;
+\i scripts/car_model_data.sql;
+\i scripts/car_model_data.sql;
+\c todo_app;
