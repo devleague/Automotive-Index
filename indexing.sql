d6d4afa15b0422d132f164e06e8a5bdb0454168b
Nikki K <808nikz@gmail.com>
2016-07-19 19:36:11 -1000

    LAM

--- indexing.sql
+++ indexing.sql
@@ -19,3 +19,16 @@ CREATE DATABASE indexed_cars WITH OWNER indexed_cars_user;
 \i scripts/car_model_data.sql;
 \i scripts/car_model_data.sql;
 
+\timing
+  SELECT DISTINCT
+    make_title
+    FROM car_models
+    WHERE make_code = 'LAM';
+
+\timing
+  SELECT DISTINCT
+    model_title
+    FROM car_models
+    WHERE make_code = 'NISSAN'
+    AND
+    model_code = 'GT-R';
