d4655b2c7e3d6ab76fdb0b04396a6b9dddc09d52
Nikki K <808nikz@gmail.com>
2016-07-20 21:30:55 -1000

    Indexed make_code, year, and model_code

--- indexing.sql
+++ indexing.sql
@@ -7,6 +7,8 @@ CREATE USER indexed_cars_user
 DROP DATABASE IF EXISTS indexed_cars;
 CREATE DATABASE indexed_cars WITH OWNER indexed_cars_user;
 
+\c indexed_cars;
+
 \i scripts/car_models.sql;
 \i scripts/car_model_data.sql;
 \i scripts/car_model_data.sql;
@@ -25,7 +27,6 @@ CREATE DATABASE indexed_cars WITH OWNER indexed_cars_user;
     FROM car_models
     WHERE make_code = 'LAM';
 
-\timing
   SELECT DISTINCT
     model_title
     FROM car_models
@@ -33,23 +34,59 @@ CREATE DATABASE indexed_cars WITH OWNER indexed_cars_user;
     AND
     model_code = 'GT-R';
 
-\timing
-  SELECT
-  make_code,
-  model_code,
-  model_title,
-  year
+  SELECT COUNT(*)
   FROM car_models
   WHERE make_code = 'LAM';
 
-\timing
-  SELECT *
+  SELECT COUNT(*)
   FROM car_models
   WHERE year 
   BETWEEN 2010
   AND 2015;
 
-\timing
-  SELECT *
+  SELECT COUNT(*)
   FROM car_models
   WHERE year = 2010;
+
+\timing
+
+CREATE INDEX index_make
+  ON car_models (
+    make_code
+    );
+CREATE INDEX index_year
+  ON car_models (
+    year
+    );
+CREATE INDEX index_model
+  ON car_models (
+    model_code
+    );
+
+\timing
+  SELECT DISTINCT
+    make_title
+    FROM car_models
+    WHERE make_code = 'LAM';
+
+  SELECT DISTINCT
+    model_title
+    FROM car_models
+    WHERE make_code = 'NISSAN'
+    AND
+    model_code = 'GT-R';
+
+  SELECT COUNT(*)
+  FROM car_models
+  WHERE make_code = 'LAM';
+
+  SELECT COUNT(*)
+  FROM car_models
+  WHERE year 
+  BETWEEN 2010
+  AND 2015;
+
+  SELECT COUNT(*)
+  FROM car_models
+  WHERE year = 2010;
+\timing
